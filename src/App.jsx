b784b32f31ea2f419cd5ee2b5511b29455a12155
aqilknz <khairunnz123@gmail.com>
2026-06-07 15:53:32 +0700

    feat: add routing for auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login"
+import Register from "./pages/Register";
+import ActivatePage from "./pages/ActivatePage";
+import Done from "./pages/Done";
+import { Toaster } from "react-hot-toast";
 
 function App() {
-
   return (
     <>
+      <Toaster position="top-right" reverseOrder={false} />
+      <Routes>
+        <Route path="auth">
+          <Route index element={<Login />} />
+          <Route path="register">
+            <Route index element={<Register />} />
+            <Route path="activate">
+              <Route index element={<ActivatePage />} />
+              <Route path="done" element={<Done />} />
+            </Route>
+          </Route>
+        </Route>
+        {/* <Route path="forgotpassword" element={<ForgotPassword />} /> */}
+      </Routes >
     </>
-    )
+  )
 }
 
 export default App
