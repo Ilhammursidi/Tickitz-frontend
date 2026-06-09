959ea18f569426dd30989a5521ba9c9861e1ee71
aqilknz <khairunnz123@gmail.com>
2026-06-09 15:36:41 +0700

    feat: add provider and persist gate

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,18 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import './global.css'
 import App from './App.jsx'
+import { Provider } from "react-redux"
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </StrictMode>,
 )
