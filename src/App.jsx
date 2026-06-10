fbcb18d25e3ef5d77c2fda3a3d8f7c16cdbaabce
Akmal Rian <akmalrian@gmail.com>
2026-06-10 12:04:40 +0700

    feat : add app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
+// import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login"
 import Register from "./pages/Register";
 import ActivatePage from "./pages/ActivatePage";
 import Done from "./pages/Done";
 import ProfilePage from "./pages/ProfilePage";
-import CheckEmail from "./pages/CheckEmail";
-import CheckOTP from "./pages/CheckOTP";
-import Reset from "./pages/ForgotPassword";
 import { Toaster } from "react-hot-toast";
 import Dashboard from "./pages/Dashboard";
-import AddMovie from "./pages/AddMovie";
-import ListMovie from "./pages/ListMovie";
 
 function App() {
   return (
@@ -26,24 +22,13 @@ function App() {
               <Route path="done" element={<Done />} />
             </Route>
           </Route>
-          <Route path="forgotpassword">
-            <Route index element={<CheckEmail />} />
-            <Route path="otp">
-              <Route index element={<CheckOTP />} />
-              <Route path="reset" element={<Reset />} />
-            </Route>
-          </Route>
         </Route>
-        <Route path="admin">
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="add-movie" element={<AddMovie />} />
-          <Route path="list-movie" element={<ListMovie />} />
-          <Route path="edit-movie/:id" element={<AddMovie />} />
-        </Route>
-        <Route path='profile' element={<ProfilePage />} />
+        {/* <Route path="forgotpassword" element={<ForgotPassword />} /> */}
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path='profile' element={<ProfilePage />}/>
       </Routes >
     </>
-  )
+  );
 }
 
-export default App
+export default App;
